08bca5433aa288eeee31b8a874d2519d6437775f
남궁찬 <ngchan03@daum.net>
2025-08-25 15:37:47 +0900

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import "../styles/Home.css";
+import "../style/Home.css";
 import Header from '../components/Header';
 import Map from '../components/Map';
 import MainMenu from '../components/MainMenu';
